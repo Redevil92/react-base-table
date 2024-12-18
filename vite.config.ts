24cadcc8da187f08acd75db1d896146e81e52160
Redevil92 <stefano.badalucco@gmail.com>
2024-12-18 18:33:27 +0100

    Updated package.json and tsconfig files to include types

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { peerDependencies } from './package.json';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { peerDependencies } from "./package.json";
 
 // https://vite.dev/config/
 export default defineConfig({
   build: {
+    outDir: "dist", // Output folder for compiled code
     lib: {
-      entry: './src/index.ts',
-      name: 'vite-react-ts-button',
+      entry: "./src/index.ts",
+      name: "vite-react-ts-button",
       fileName: (format) => `index.${format}.js`,
-      formats: ['cjs', 'es'],
+      formats: ["cjs", "es"],
     },
     rollupOptions: {
       external: [...Object.keys(peerDependencies)],
