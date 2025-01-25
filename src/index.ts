a73754b17504daeaa61256cd8c9e11eb0afcc714
Redevil92 <stefano.badalucco@gmail.com>
2025-01-25 17:54:55 +0100

    Tidy up

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import "./output.css";
 
 // export { default as BaseTable } from "./components/BaseTable/BaseTable";
 
-// export { default as BaseButton } from "./components/BaseButton";
+//export { default as BaseButton } from "./components/BaseButton";
 
 export { default as BaseTable } from "./components/BaseTable/BaseTable";
-
-// export const TEST = "Hello";
