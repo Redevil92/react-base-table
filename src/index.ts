237396348a57c13d1ce811337c7ead72821c9eda
Redevil92 <stefano.badalucco@gmail.com>
2025-07-05 23:00:14 +0200

    Added models in index.ts (in order to export them).

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,15 @@ import "./output.css";
 // export { default as BaseTable } from "./components/BaseTable/BaseTable";
 
 export { default as BaseTable } from "./components/BaseTable/BaseTable";
+
+import ActiveTableFilter from "./components/BaseTable/models/ActiveTableFilter";
+export type { ActiveTableFilter };
+
+import BaseTableHeader from "./components/BaseTable/models/BaseTableHeaders";
+export type { BaseTableHeader };
+
+import TableConfiguration from "./components/BaseTable/models/TableConfiguration";
+export type { TableConfiguration };
+
+// import TableItem from "./components/BaseTable/models/TableItem";
+// export type { TableItem };
